c558c4aa9a22683ad5cc98c505dcbcc95f32e621
Reza Ahmadi <r.odises@gmail.com>
2016-09-09 19:19:06 +0430

    validation logic for passwords #21

--- src/Kondor.WebApplication/App_Start/IdentityConfig.cs
+++ src/Kondor.WebApplication/App_Start/IdentityConfig.cs
@@ -55,10 +55,10 @@ namespace Kondor.WebApplication
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
