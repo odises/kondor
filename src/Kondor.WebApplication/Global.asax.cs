9ef9d2a23e9a12e3e3587bba92c782374425d044
Reza Ahmadi <r.odises@gmail.com>
2016-09-24 17:48:57 +0330

    IoC applied in the Web-Application. This closes #61

--- src/Kondor.WebApplication/Global.asax.cs
+++ src/Kondor.WebApplication/Global.asax.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Kondor.Data;
 
 namespace Kondor.WebApplication
 {
@@ -12,6 +15,18 @@ namespace Kondor.WebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof (MvcApplication).Assembly);
+
+            builder.RegisterType<KondorDataContext>().As<IDbContext>();
+
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            
         }
     }
 }
